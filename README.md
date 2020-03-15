d0a293a7b1ad46151d7ec70b6f7eb82e642dd8c2
matthewgorbett <Idontknow8112@@@>
2020-03-15 10:45:01 -0600

    more

--- README.md
+++ README.md
@@ -4,3 +4,12 @@ Run:
 pip install -r requirements.txt
 
 wen-add any new libraries to the requirements file
+
+
+data_analysis_initial:
+
+trying to replicate RDA in paper: 
+https://arxiv.org/pdf/1809.01281.pdf
+page 20
+
+on only first two imagenet images 
--- data_analysis_initial.py
+++ data_analysis_initial.py
@@ -1,7 +1,8 @@
 import h5py
 import os
 from skimage import io
-from torchvision import transforms
+from torchvision import transforms, models
+import torch
 
 filename = './ROIs/CSI1/h5/CSI1_ROIs_TR1.h5'
 
@@ -23,13 +24,36 @@ f = open('./ROIs/stim_lists/CSI01_stim_lists.txt', 'r')
 CSI01_stim_lists = f.read().splitlines()
 f.close()
 
-print(CSI01_stim_lists[1])
-print(CSI01_stim_lists[2])
-
+#first image
 image_path_full1=os.path.join(image_folder, CSI01_stim_lists[0])
 image = io.imread(image_path_full1)
-sample = image
 to_tensor = transforms.Compose([transforms.ToTensor()])
+image1=to_tensor(image)
 
-sample=to_tensor(sample)
-print(sample)
+
+#second image
+image_path_full1=os.path.join(image_folder, CSI01_stim_lists[1])
+image = io.imread(image_path_full1)
+to_tensor = transforms.Compose([transforms.ToTensor()])
+image2=to_tensor(image)
+
+
+alexnet = models.alexnet(pretrained=True)
+
+alexnet.features[:2]#cnn1
+alexnet.features[:5]#cnn2
+alexnet.features[:8]#cnn3
+alexnet.features[:10]#cnn4
+alexnet.features[:12]#cnn5
+
+out=alexnet.features(image1.unsqueeze(dim=0))
+out=alexnet.avgpool(out)
+out = torch.flatten(out, 1)
+out=alexnet.classifier[:3](out)#fc6
+
+out=alexnet.features(image1.unsqueeze(dim=0))
+out=alexnet.avgpool(out)
+out = torch.flatten(out, 1)
+out=alexnet.classifier[:6](out)#fc7
+
+
