6d0b37bd08c85622b5bf3dfe285d9551bfbb3674
matthewgorbett <Idontknow8112@@@>
2020-03-17 13:53:30 -0600

    cuda cuda cuda

--- dataloaders/cifar10_dataloaders.py
+++ dataloaders/cifar10_dataloaders.py
@@ -3,9 +3,12 @@ from torchvision import *
 
 def get_cifar_dataloaders(batch_size):
 
-    transform = transforms.Compose(
-        [transforms.ToTensor(),
-         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    transform = transforms.Compose([
+         transforms.RandomHorizontalFlip(),transforms.ToTensor(),
+         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
+
+    transform_test = transforms.Compose([transforms.ToTensor(),
+         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
 
     trainset = datasets.CIFAR10(root='./cifar10_data', train=True,
                                             download=True, transform=transform)
@@ -13,7 +16,7 @@ def get_cifar_dataloaders(batch_size):
                                               shuffle=True, num_workers=2)
 
     testset = datasets.CIFAR10(root='./cifar10_data', train=False,
-                                           download=True, transform=transform)
+                                           download=True, transform=transform_test)
     testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
                                              shuffle=False, num_workers=2)
 
